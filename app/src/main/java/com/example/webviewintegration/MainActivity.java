2bf66bdb7ddaab496a4c204e8b8c900052263ce7
Jooker302 <i.omer6637@gmail.com>
2023-06-11 23:28:14 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/webviewintegration/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.webviewintegration;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.appcompat.widget.SearchView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private WebViewFragment webViewFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        webViewFragment = new WebViewFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, webViewFragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                loadWebViewFragment(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return true;
+    }
+
+    private void loadWebViewFragment(String url) {
+        webViewFragment.loadUrl(url);
+    }
+}
---
+++ app/src/main/java/com/example/webviewintegration/WebViewFragment.java
@@ -0,0 +1,113 @@
+package com.example.webviewintegration;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.widget.SearchView;
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WebViewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WebViewFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private WebView webView;
+
+    public WebViewFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment WebViewFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WebViewFragment newInstance(String param1, String param2) {
+        WebViewFragment fragment = new WebViewFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+
+        webView = view.findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                Log.d("WebView", "Page started loading: " + url);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.d("WebView", "Page finished loading: " + url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                Log.e("WebView", "Error loading page: " + error.getDescription());
+            }
+        });
+
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            String url = bundle.getString("url");
+            loadUrl(url);
+        }
+
+        return view;
+    }
+
+    public void loadUrl(String url) {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "https://" + url;
+        }
+        webView.loadUrl(url);
+    }
+}
